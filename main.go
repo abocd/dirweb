71e8914d317b1f5b6259d6cc71c1deb3e81982c2
风子 <abocd@qq.com>
2019-02-14 09:42:54 +0800

    增加了没有找到端口时的提示

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main()  {
 		log.Fatal(err)
 	}
 	fmt.Println("程序：aboc (http://www.phpec.org)")
-	fmt.Println("如果使用存在问题，请访问 https://github.com/abocd/dirweb/releases 提交问题")
+	fmt.Println("如果使用存在问题，请访问 https://github.com/abocd/dirweb 提交问题")
 	fmt.Println(fmt.Sprintf("文件目录位于 %s",dir))
 	http.Handle("/",http.FileServer(http.Dir(fmt.Sprintf("%s/",dir))))
 
@@ -25,12 +25,17 @@ func main()  {
 	var findPort chan int
 	findPort = make(chan int ,1)
 	go func(){
+		var havePort = false
 		for port := 8080;port <=9000;port++{
 			_,err = net.Dial("tcp",fmt.Sprintf(":%d",port))
 			if err != nil{
 				findPort <- port
+				havePort = true
 				break
 			}
+		}
+		if !havePort{
+			fmt.Println("沒有找到合适的端口用来启动服务器，请关掉重新打开")
 		}
 	}()
 	serverPort := <-findPort
