5f6c71c66ffe6a8330e98b6760d1c29b873211a2
aboc <abocd@qq.com>
2019-04-22 10:08:43 +0800

    增加浏览器自动打开

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
-	"path/filepath"
-	"os"
 	"log"
-	"net/http"
 	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
 )
 
-func main()  {
+func main() {
 
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
@@ -17,30 +19,47 @@ func main()  {
 	}
 	fmt.Println("程序：aboc (http://www.phpec.org)")
 	fmt.Println("如果使用存在问题，请访问 https://github.com/abocd/dirweb/releases 提交问题")
-	fmt.Println(fmt.Sprintf("文件目录位于 %s",dir))
-	http.Handle("/",http.FileServer(http.Dir(fmt.Sprintf("%s/",dir))))
-
+	fmt.Println(fmt.Sprintf("文件目录位于 %s", dir))
+	http.Handle("/", http.FileServer(http.Dir(fmt.Sprintf("%s/", dir))))
 
 	//fmt.Println(err)
 	var findPort chan int
-	findPort = make(chan int ,1)
-	go func(){
-		for port := 8080;port <=9000;port++{
-			_,err = net.Dial("tcp",fmt.Sprintf(":%d",port))
-			if err != nil{
+	findPort = make(chan int, 1)
+	go func() {
+		for port := 8080; port <= 9000; port++ {
+			_, err = net.Dial("tcp", fmt.Sprintf(":%d", port))
+			if err != nil {
 				findPort <- port
 				break
 			}
 		}
 	}()
 	serverPort := <-findPort
-	startServer(serverPort)
+	go startServer(serverPort)
+	url := fmt.Sprintf("http://127.0.0.1:%d/", serverPort)
+	time.Sleep(3 * time.Second)
+	fmt.Println("准备打开浏览器...")
+	runUrl(url)
+	<-findPort
 }
 
-func startServer(port int)error{
-	fmt.Println(fmt.Sprintf("服务器启动成功，请访问 http://127.0.0.1:%d/",port))
-	if err := http.ListenAndServe(fmt.Sprintf(":%d",port),nil);err != nil{
+func startServer(port int) error {
+	fmt.Println(fmt.Sprintf("文件服务器启动成功，请访问 http://127.0.0.1:%d/", port))
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		return err
 	}
 	return nil
+}
+
+func runUrl(url string) (err2 error, msg string) {
+	cmd := exec.Command("explorer", url)
+
+	err := cmd.Run()
+	if err != nil {
+		// fmt.Println("启动失败:", err)
+		return err, "启动失败"
+	} else {
+		// fmt.Println("启动成功!")
+		return nil, "启动成功"
+	}
 }
